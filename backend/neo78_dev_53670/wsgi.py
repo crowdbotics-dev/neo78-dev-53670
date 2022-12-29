8526ff71c8099860780c25cded06b812b4a74ec0
Crowdbotics <engineering@crowdbotics.com>
2022-12-29 17:03:45 +0000

    Initial commit.

---
+++ backend/neo78_dev_53670/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for neo78_dev_53670 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neo78_dev_53670.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "neo78-dev-53670",
+  url: "https://neo78-dev-53670.botics.co",
+  api: "https://neo78-dev-53670.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
